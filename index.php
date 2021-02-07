f62eab5682c373495b5040c9e35ca9f41f108daa
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-02-07 07:55:47 -0500

    Set up different routes for my future pages

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //require the autoload file
 require_once('vendor/autoload.php');
 
@@ -22,6 +25,33 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 }
 );
+
+//order route
+$f3->route('GET /profile', function(){
+    //echo "Order Page";
+    $view = new Template();
+  //  echo $view->render('views/profile.html');
+    echo "Profile";
+});
+
+//order2 route
+$f3->route('POST /interests', function(){
+
+
+    //display a view
+    $view = new Template();
+    echo $view->render('views/interests.html');
+
+});
+
+$f3->route('POST /summary', function(){
+
+
+    //display a view
+    $view = new Template();
+    echo $view->render('views/summary.html');
+
+});
 
 //run fat free
 $f3->run();
