7f8a1f2907ab5ebf369b6dbb5e0be4f2384fc067
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-02-21 18:35:22 -0500

    Added data-layer

---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+/*model/data-layer.php
+*/
+
+function getIndoorInterests() {
+    return array("tv","puzzles","movies","reading","cooking","playing cards","board games", "video games");
+}
+
+function getOutdoorInterests() {
+    return array("hiking", "walking", "biking","climbing","swimming","camping", "skiing","rafting");
+}
