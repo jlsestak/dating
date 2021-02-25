a1bd70e369ca7cc3f47b87d208fcd8e4f35d0188
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-02-24 22:39:46 -0500

    Added validation for age, phone, email and interests

--- model/validation.php
+++ model/validation.php
@@ -3,35 +3,55 @@
 Contains validation for Jessica's dating site
 
 */
+//require data-layer
+require_once('data-layer.php');
 
+//Checks to see if user's names are alphabetic and are not empty
 function validName($name)
 {
-    $name = trim($name);
     return !empty($name) && ctype_alpha($name);
 
 }
 
-function validAge()
+//Checks to see if the user's age is not empty, between 18 and 118 and is numeric
+function validAge($age)
 {
-
+    return !empty($age) && $age >= 18 && $age <= 118 && is_numeric($age);
 }
 
-function validPhone()
+//Checks to see if the user has a valid phone (xxx-xxx-xxxx)
+function validPhone($phone)
 {
-
+    return !empty($phone) && preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone);
 }
 
-function validEmail()
+//checks to see if the email is valid
+function validEmail($email)
 {
-
+    return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
-function validOutdoor()
+//checks to see if the outdoor interest values are valid
+function validOutdoor($outdoor)
 {
+    $validOutdoor = getOutdoorInterests();
+    foreach ($outdoor as $outdoorInterests) {
 
+        if (!in_array($outdoorInterests, $validOutdoor)) {
+            return false;
+        }
+    }
+    return true;
 }
 
-function validIndoor()
+//checks to see if the indoor interest values are valid
+function validIndoor($indoor)
 {
-
+    $validIndoor = getIndoorInterests();
+    foreach ($indoor as $indoorInterests) {
+        if (!in_array($indoorInterests, $validIndoor)) {
+            return false;
+        }
+    }
+    return true;
 }
