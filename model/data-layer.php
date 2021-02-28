f62918c10a47eeaf881265c74f4c03a367b561e0
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-02-27 21:09:16 -0500

    Created classes for validate and datalayer

--- model/data-layer.php
+++ model/data-layer.php
@@ -3,10 +3,30 @@
 /*model/data-layer.php
 */
 
-function getIndoorInterests() {
-    return array("tv","puzzles","movies","reading","cooking","playing cards","board games", "video games");
-}
+class DataLayer
+{
+    /**
+     * getGender returns an array of genders
+     * @return string[]
+     */
+    function getGender() {
+        return array ("female", "male");
+    }
 
-function getOutdoorInterests() {
-    return array("hiking", "walking", "biking","climbing","swimming","camping", "skiing","rafting");
+    /**
+     * getIndoorInterests() returns an array of indoor interests
+     * @return string[]
+     */
+    function getIndoorInterests() {
+        return array("tv","puzzles","movies","reading","cooking","playing cards","board games", "video games");
+    }
+
+    /**
+     * getOutdoorInterest returns an array of outdoor interests
+     * @return string[]
+     */
+    function getOutdoorInterests() {
+        return array("hiking", "walking", "biking","climbing","swimming","camping", "skiing","rafting");
+    }
+
 }
--- model/validation.php
+++ model/validation.php
@@ -3,55 +3,91 @@
 Contains validation for Jessica's dating site
 
 */
-//require data-layer
-require_once('data-layer.php');
 
-//Checks to see if user's names are alphabetic and are not empty
-function validName($name)
+class Validate
 {
-    return !empty($name) && ctype_alpha($name);
 
-}
+    private $_dataLayer;
 
-//Checks to see if the user's age is not empty, between 18 and 118 and is numeric
-function validAge($age)
-{
-    return !empty($age) && $age >= 18 && $age <= 118 && is_numeric($age);
-}
-
-//Checks to see if the user has a valid phone (xxx-xxx-xxxx)
-function validPhone($phone)
-{
-    return !empty($phone) && preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone);
-}
-
-//checks to see if the email is valid
-function validEmail($email)
-{
-    return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-//checks to see if the outdoor interest values are valid
-function validOutdoor($outdoor)
-{
-    $validOutdoor = getOutdoorInterests();
-    foreach ($outdoor as $outdoorInterests) {
-
-        if (!in_array($outdoorInterests, $validOutdoor)) {
-            return false;
-        }
+    function __construct()
+    {
+        $this->_dataLayer = new DataLayer();
     }
-    return true;
-}
 
-//checks to see if the indoor interest values are valid
-function validIndoor($indoor)
-{
-    $validIndoor = getIndoorInterests();
-    foreach ($indoor as $indoorInterests) {
-        if (!in_array($indoorInterests, $validIndoor)) {
-            return false;
-        }
+    /**
+     * validName() Checks to see if user's names are alphabetic and are not empty
+     * @param String $name
+     * @return bool
+     */
+    function validName($name)
+    {
+        return !empty($name) && ctype_alpha($name);
+
     }
-    return true;
+
+
+    /**
+     * validAge Checks to see if the user's age is not empty, between 18 and 118 and is numeric
+     * @param Integer $age
+     * @return bool
+     */
+    function validAge($age)
+    {
+        return !empty($age) && $age >= 18 && $age <= 118 && is_numeric($age);
+    }
+
+
+    /**
+     *validPhone() Checks to see if the user has a valid phone (xxx-xxx-xxxx)
+     * @param String $phone
+     * @return bool
+     */
+    function validPhone($phone)
+    {
+        return !empty($phone) && preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone);
+    }
+
+
+    /**
+     * validEmail() Checks to see if the user has a valid email
+     * @param String $email
+     * @return bool
+     */
+    function validEmail($email)
+    {
+        return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * validOutdoor checks to see if the outdoor interest values are valid
+     * @param array $outdoor
+     * @return bool
+     */
+    function validOutdoor($outdoor)
+    {
+        $validOutdoor = getOutdoorInterests();
+        foreach ($outdoor as $outdoorInterests) {
+
+            if (!in_array($outdoorInterests, $validOutdoor)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * validIndoor() checks to see if the indoor interest values are valid
+     * @param array $indoor
+     * @return bool
+     */
+    function validIndoor($indoor)
+    {
+        $validIndoor = getIndoorInterests();
+        foreach ($indoor as $indoorInterests) {
+            if (!in_array($indoorInterests, $validIndoor)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
