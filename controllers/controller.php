bcf003d45cfe74c1034249187ae6053ba4f89638
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-03-05 01:24:11 -0500

    Fixed controllers

--- controllers/controller.php
+++ controllers/controller.php
@@ -3,6 +3,7 @@
 class Controller
 {
     private $_f3;
+    private $_memberRank;
 
     function __construct($f3)
     {
@@ -20,10 +21,12 @@ class Controller
     /*Display personal page */
     function personal()
     {
+        global $_memberRank;
         global $validator;
+        global $datalayer;
 
         //send the array of genders to the personal view
-        $this->_f3->set('genders', getGender());
+        $this->_f3->set('genders', $datalayer->getGender());
 
         //Check to see if the user has submitted the personal page
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -36,8 +39,8 @@ class Controller
             $phone = $_POST['phone'];
 
             //Save first name to session if valid
-            if (validName($fname)) {
-                $_SESSION['fname'] = $_POST['fname'];
+            if ($validator->validName($fname)) {
+                $fname = $_POST['fname'];
             }
             else if($fname ==""){
                 $this->_f3->set('errors["fname"]', "First Name cannot be blank");
@@ -48,7 +51,7 @@ class Controller
 
             //Save last name to session if valid
             if ($validator->validName($lname)) {
-                $_SESSION['lname'] = $_POST['lname'];
+                $lname = $_POST['lname'];
             }
             else if($lname == ""){
 
@@ -60,7 +63,7 @@ class Controller
 
             //Save age if valid
             if ($validator->validAge($age)) {
-                $_SESSION['age'] = $_POST['age'];
+                $age = $_POST['age'];
             }
             else if(!is_numeric($age)) {
                 $this->_f3->set('errors["age"]', "Please enter a numeric value for age");
@@ -79,12 +82,12 @@ class Controller
 
             //save gender to session
             if (isset($this->_f3->$_POST['genders'])) {
-                $_SESSION['gender'] = $_POST['genders'];
+                $gender = $_POST['genders'];
             }
 
             //save phone to session
             if ($validator->validPhone($phone)) {
-                $_SESSION['phone'] = $_POST['phone'];
+                $phone = $_POST['phone'];
             }
             else if($phone == "") {
                 $this->_f3->set('errors["phone"]', "Phone number cannot be blank");
@@ -93,9 +96,21 @@ class Controller
                 $this->_f3->set('errors["phone"]', "Please put a valid phone number (XXX-XXX-XXXX)");
             }
 
+            //Save premiumMember checkbox
+            if(isset($_POST['premiumMember'])) {
+                $_SESSION['premiumMember'] = $_POST['premiumMember'];
+            }
+
             //If there are no errors, redirect to /profile
             if(empty($this->_f3->get('errors'))) {
-                $this->_f3->reroute('/profile');
+                if($_SESSION['premiumMember']){
+                    $_memberRank = new PremiumMember($fname, $lname, $age, $gender, $phone);
+                }
+                else {
+                    $_memberRank = new Member($fname, $lname, $age, $gender, $phone);
+                }
+                $_SESSION['memberRank'] = $_memberRank;
+                $this->_f3->reroute('profile');
             }
 
         }
@@ -106,6 +121,7 @@ class Controller
         $this->_f3->set('userAge', isset($age) ? $age : "");
         $this->_f3->set('userGender', isset($gender) ? $gender : "");
         $this->_f3->set('userPhone', isset($phone) ? $phone : "");
+        $this->_f3->set('premiumMember', isset($premiumMember) ? $premiumMember : "");
 
         $view = new Template();
         echo $view->render('views/personal.html');
@@ -113,7 +129,9 @@ class Controller
 
     function profile()
     {
+        global $_memberRank;
         global $datalayer;
+        global $validator;
         //send the gender array to the profile page
         $this->_f3->set('seekingGender', $datalayer->getGender());
 
@@ -127,34 +145,40 @@ class Controller
             $state = $_POST['state'];
 
             //save email to session
-            if (validEmail($email)) {
-                $_SESSION['email'] = $_POST['email'];
-            }
-            else if($email == "") {
+            if ($validator->validEmail($email)) {
+                $email = $_POST['email'];
+            } else if ($email == "") {
                 $this->_f3->set('errors["email"]', "Email cannot be blank");
-            }
-            else {
+            } else {
                 $this->_f3->set('errors["email"]', "Please give a valid email");
             }
 
             //save state to session
-            if (isset($this->_f3->$_POST['state'])) {
-                $_SESSION['state'] = $_POST['state'];
+            if (isset($_POST['state'])) {
+                $state = $_POST['state'];
             }
             //save seeking to session
-            if (isset($this->_f3->$_POST['seeking'])) {
-                $_SESSION['seeking'] = $_POST['seeking'];
+            if (isset($_POST['seeking'])) {
+                $seeking = $_POST['seeking'];
             }
             //save biography to session
-            if (isset($this->_f3->$_POST['biography'])) {
-                $_SESSION['biography'] = $_POST['biography'];
+            if (isset($_POST['biography'])) {
+                $biography = $_POST['biography'];
             }
-            //If there are no errors, redirect to /profile
-            if(empty($this->_f3->set('errors'))) {
-                $this->_f3->reroute('/interests');
-            }
-        }
 
+
+
+            //If there are no errors, redirect to /profile
+            if (empty($this->_f3->get('errors'))) {
+                //store the data in an object
+                $_memberRank->setEmail($email);
+                $_memberRank->setState($state);
+                $_memberRank->setSeeking($seeking);
+                $_memberRank->setBio($biography);
+                $this->_f3->reroute('interests');
+            }
+
+        }
         //Make the profile form sticky
         $this->_f3->set('userEmail', isset($email) ? $email : "");
         $this->_f3->set('userSeeking', isset($seeking) ? $seeking : "");
@@ -168,6 +192,10 @@ class Controller
 
     function interests()
     {
+        if(!$_SESSION['premiumMember']) {
+            $this->_f3->reroute('/summary');
+        }
+        global $_memberRank;
         global $validator;
         global $datalayer;
         //Check to see if the user has submitted the interest page
@@ -181,7 +209,7 @@ class Controller
             if(isset($_POST['indoorInterests'])) {
                 if ($validator->validIndoor($indoorInterests)) {
                     $indoorInterests = implode(", ", $indoorInterests);
-                    $_SESSION['indoorInterests'] = $indoorInterests;
+
                 } else {
                     $this->_f3->set('errors["indoors"]', "Please select a valid indoor interest");
                 }
@@ -191,12 +219,16 @@ class Controller
             if(isset($outdoorInterests)) {
                 if($validator->validOutdoor($outdoorInterests)) {
                     $outdoorInterests = implode(", ",$outdoorInterests);
-                    $_SESSION['outdoorInterests'] = $outdoorInterests;
+
                 }
                 else {
                     $this->_f3->set('errors["outdoors"]', "Please select a valid outdoor interest");
                 }
             }
+            //store the data in an object
+            $_memberRank->setIndoorInterests($indoorInterests);
+            $_memberRank->setOutdoorInterests($outdoorInterests);
+
 
             //If there are no errors, redirect to /profile
             if(empty($this->_f3->get('errors'))) {
@@ -205,8 +237,8 @@ class Controller
         }
 
         //send the interest arrays to the interest form
-        $this->_f3->set('indoor', getIndoorInterests());
-        $this->_f3->set('outdoor', getOutdoorInterests());
+        $this->_f3->set('indoor', $datalayer->getIndoorInterests());
+        $this->_f3->set('outdoor', $datalayer->getOutdoorInterests());
 
         //display interests view
         $view = new Template();
